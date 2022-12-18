3c258dfce42efad52d1873e4e7d70676657c1b10
abdoachhoubi <aachhoub@gmail.com>
2022-12-18 22:56:10 +0100

    Moved all source files into src dir

---
+++ src/index.js
@@ -0,0 +1,3 @@
+"use strict";
+// We can annotate a variable by adding the ":" after its name and add it's type
+var n = 12;
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+"use strict";
+
+// We can annotate a variable by adding the ":" after its name and add it's type
+let n: number = 12;
+
+// Now we can't assign another data type to n
+// n = "hi"; Type 'string' is not assignable to type 'number'.
